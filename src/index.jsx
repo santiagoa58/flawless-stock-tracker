d8ead671439f28fda588bb861d9882a282af0abe
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-07-02 16:52:28 -0400

    change to use jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  React.createElement(App, { title: 'Stock Trader App' }),
+  <App title={'Stock Tracker App'} />,
   document.getElementById('root')
 );
