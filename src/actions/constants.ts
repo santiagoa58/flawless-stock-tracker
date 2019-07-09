c31945735639fdc3944b39e4e4ceb585efe0ebb4
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-07-09 18:58:00 -0400

    add new constants

--- src/actions/constants.ts
+++ src/actions/constants.ts
@@ -1,6 +1,22 @@
 export const SET_NEWS = 'SET_NEWS';
-export const SET_NEWS_ERROR = 'SET_NEWS_ERROR';
 export const GET_NEWS = 'GET_NEWS';
+export const SET_NEWS_ERROR = 'SET_NEWS_ERROR';
+
 export const SET_OVERVIEW = 'SET_OVERVIEW';
-export const SET_OVERVIEW_ERROR = 'SET_OVERVIEW_ERROR';
 export const GET_OVERVIEW = 'GET_OVERVIEW';
+export const SET_OVERVIEW_ERROR = 'SET_OVERVIEW_ERROR';
+
+export const SET_TOP_PEERS = 'SET_TOP_PEERS';
+export const GET_TOP_PEERS = 'GET_TOP_PEERS';
+export const SET_TOP_PEERS_ERROR = 'SET_TOP_PEERS_ERROR';
+
+export const SET_KEY_STATS = 'SET_KEY_STATS';
+export const GET_KEY_STATS = 'GET_KEY_STATS';
+export const SET_KEY_STATS_ERROR = 'SET_KEY_STATS_ERROR';
+
+export const SET_TIME_SERIES = 'SET_TIME_SERIES';
+export const GET_TIME_SERIES = 'GET_TIME_SERIES';
+export const SET_TIME_SERIES_ERROR = 'SET_TIME_SERIES_ERROR';
+
+export const SEARCH = 'SEARCH';
+export const SET_SEARCH_ERROR = 'SET_SEARCH_ERROR';
