04af3dc42a3b8db978f84350820bc462dac210e5
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-07-09 19:01:07 -0400

    clean up top level components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,41 +1,11 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
+import { HeaderContainer } from './header/headerContainer';
 
-import { getNews, NewsAction } from '../actions/news-actions';
-import { ThunkDispatch } from 'redux-thunk';
-import { NewsState } from '../reducers/news-reducer';
-import { OverviewState } from '../reducers/overview-reducer';
-
-interface AppProps {
-  title: string;
-  news: any[];
-  gNews: () => void;
-}
-
-interface RootReducer {
-  newsReducer: NewsState;
-  overviewReducer: OverviewState;
-}
-
-const App = ({ title, news, gNews }: AppProps) => {
+export const App: React.SFC = () => {
   return (
     <>
-      <h1>{title}</h1>
-      <button onClick={gNews}>click me to see great news</button>
-      {news && <p>{news[0].headline}</p>}
+      <h1>title</h1>
+      <HeaderContainer title="Stock Tracker App" />
     </>
   );
 };
-
-const mapStateToProps = ({ newsReducer }: RootReducer) => ({
-  news: newsReducer.news,
-});
-
-const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, NewsAction>) => ({
-  gNews: () => dispatch(getNews('aapl')),
-});
-
-export const DecoratedApp = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import { configureStore } from './store';
-import { DecoratedApp } from './components/App';
+import { App } from './components/App';
 
 const store = configureStore();
 
 render(
   <Provider store={store}>
-    <DecoratedApp title={'amazing page'} />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
