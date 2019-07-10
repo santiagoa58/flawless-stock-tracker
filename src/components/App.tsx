95d6c5156d72afe631fffd5695891cdeb36ad220
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-07-10 16:24:58 -0400

    add new components to app component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { HeaderContainer } from './header/headerContainer';
+import { NewsGraphContainer } from './news-graph-layout/newsGraphContainer';
+import { OverviewStatsContainer } from './overview-stats-layout/overviewStatsContainer';
 
 export const App: React.SFC = () => {
   return (
     <>
       <HeaderContainer title="Stock Tracker App" />
+      <NewsGraphContainer />
+      <OverviewStatsContainer />
     </>
   );
 };
