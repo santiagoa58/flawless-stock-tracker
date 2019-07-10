f4370e1bc56db64cfb64f903ebc3197495d8ff89
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-07-10 09:04:02 -0400

    remove hardcoded title

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,6 @@ import { HeaderContainer } from './header/headerContainer';
 export const App: React.SFC = () => {
   return (
     <>
-      <h1>title</h1>
       <HeaderContainer title="Stock Tracker App" />
     </>
   );
