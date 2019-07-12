7a3f81ce7a6041677535a86b930f48dbb8ff1073
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-07-12 15:09:29 -0400

    remove unused import

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,6 +1,5 @@
 export { getOverview } from './overview-selectors';
 export { getQuote } from './quote-selectors';
 export { getNews } from './news-selectors';
-export { getTimeSeries } from './time-series-selectors';
 export { getPeers } from './peers-selectors';
 export { getKeyStats } from './keystats-selectors';
