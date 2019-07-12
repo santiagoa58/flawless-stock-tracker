fa484076d5a6683b8728e552832746ccd99978d6
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-07-12 16:42:47 -0400

    add change graph timeFrame functionality

--- src/components/graph/graph.tsx
+++ src/components/graph/graph.tsx
@@ -1,75 +1,48 @@
 import * as React from 'react';
-import {
-  ResponsiveContainer,
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-} from 'recharts';
-import { TimeSeries } from '../../util';
+import { TimeSeries, TimeSeriesRange } from '../../util';
+import { GraphDisplay } from './graphDisplay';
 
-interface GraphProps {
-  oneDay: TimeSeries;
-  oneMonth: TimeSeries;
-  fiveDay: TimeSeries;
-  oneYear: TimeSeries;
-  fiveYear: TimeSeries;
-  max: TimeSeries;
-}
+type GraphProps = {
+  [key: string]: TimeSeries;
+};
 
-export const Graph: React.SFC<GraphProps> = ({
-  oneDay,
-  oneMonth,
-  fiveDay,
-  oneYear,
-  fiveYear,
-  max,
-}: GraphProps) => {
+export const Graph: React.SFC<GraphProps> = (props: GraphProps) => {
+  const [dataChoice, setData] = React.useState('oneDay');
+
+  function selectChartData(dataSelect: string) {
+    setData(dataSelect);
+  }
+
   return (
     <div className="chart">
-      {oneDay[0] && (
-        <ResponsiveContainer width="100%" aspect={2}>
-          <AreaChart
-            data={oneMonth}
-            margin={{
-              top: 0,
-              right: 0,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <defs>
-              <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="15%" stopColor="#8884d8" stopOpacity={1} />
-                <stop offset="85%" stopColor="#8884d8" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey={'label'}
-              interval={'preserveStart'}
-              tick={{ stroke: '#f4f6f9', strokeWidth: 0.1 }}
-            />
-            <YAxis
-              orientation="right"
-              domain={['dataMin', 'auto']}
-              tick={{ stroke: '#f4f6f9', strokeWidth: 0.1 }}
-            />
-            <Tooltip
-              cursor={{ stroke: 'orange', strokeWidth: 2 }}
-              labelStyle={{ color: 'black' }}
-            />
-            <Area
-              type="monotone"
-              dataKey={'close'}
-              stroke="#8884d8"
-              fill="url(#colorPrice)"
-              fillOpacity={0.3}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
+      {props.oneDay[0] && (
+        <>
+          <div className="chart__select">
+            <a href="#" onClick={() => selectChartData('oneDay')}>
+              {'1D'}
+            </a>
+            <a href="#" onClick={() => selectChartData('fiveDay')}>
+              {'5D'}
+            </a>
+            <a href="#" onClick={() => selectChartData('oneMonth')}>
+              {'1M'}
+            </a>
+            <a href="#" onClick={() => selectChartData('oneYear')}>
+              {'1Y'}
+            </a>
+            <a href="#" onClick={() => selectChartData('fiveYear')}>
+              {'5Y'}
+            </a>
+            <a href="#" onClick={() => selectChartData('max')}>
+              {'MAX'}
+            </a>
+          </div>
+          <GraphDisplay
+            data={props[`${dataChoice}`]}
+            dataName="close"
+            timeFrame="label"
+          />
+        </>
       )}
     </div>
   );
