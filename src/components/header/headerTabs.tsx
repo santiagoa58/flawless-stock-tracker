e5945c083317d809c984e6fef5279ab16573adf2
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-07-12 16:42:25 -0400

    modify header styling

--- src/components/header/headerTabs.tsx
+++ src/components/header/headerTabs.tsx
@@ -3,7 +3,9 @@ import * as React from 'react';
 export const HeaderTabs = () => {
   return (
     <div className="header-tabs">
-      <a href="#">QUOTES</a>
+      <a className="header-tabs--active" href="#">
+        QUOTES
+      </a>
       <a href="#">MARKETS</a>
       <a href="#">WATCHLISTS</a>
     </div>
